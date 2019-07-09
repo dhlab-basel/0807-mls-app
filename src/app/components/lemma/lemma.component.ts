607b34a443125449e3026a24603ad9924372e60b
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2019-07-09 14:22:39 +0200

    Starting Lemma view

---
+++ src/app/components/lemma/lemma.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lemma',
+  template: `
+    <p>
+      lemma works!
+    </p>
+  `,
+  styles: []
+})
+export class LemmaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
