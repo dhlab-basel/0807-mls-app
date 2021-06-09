6c0a2b1acdbfc37e33892e8c06644b494daa2b17
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2021-06-09 17:28:26 +0200

    Ready for departure - all aboard!

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -50,8 +50,8 @@ export class LoginComponent implements OnInit {
               @Inject(MAT_DIALOG_DATA) data: LoginData) { }
 
   ngOnInit() {
-    this.email = 'lukas.rosenthaler@gmail.com';
-    this.password = 'RioGrande2020';
+    // this.email = '';
+    // this.password = '';
     this.form = this.fb.group({
       email: [this.email, []],
       password: [this.password, []]
